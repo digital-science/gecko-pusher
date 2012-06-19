721a4a37364d59152f39073a5a65dba28a4aea1a
Steven Mohapi-Banks <steven.mohapibanks@gmail.com>
2012-06-19 17:20:47 +0100

    Now supports basic line charts

--- lib/gecko-pusher/channel/line_chart.rb
+++ lib/gecko-pusher/channel/line_chart.rb
@@ -4,7 +4,10 @@ module Gecko
       class LineChart < Base
 
         def push(*args)
-
+          data = {
+            item: args
+          }
+          _push(data)
         end
 
       end
--- spec/lib/gecko-pusher/channel/line_chart_spec.rb
+++ spec/lib/gecko-pusher/channel/line_chart_spec.rb
@@ -12,4 +12,13 @@ describe Gecko::Pusher::Channel::LineChart do
     @channel.should be_a(Gecko::Pusher::Channel::LineChart)
   end
 
+  it "should push a basic array of values" do
+    data = {
+      item: [ 1,2,3,4,5 ]
+    }
+    stub = stub_gecko_post(WIDGET_KEY, data)
+    @channel.push(1,2,3,4,5)
+    stub.should have_been_requested
+  end
+
 end
