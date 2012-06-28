2673c3cd42533bec645b7763f2b0684f1da66bd6
Steven Mohapi-Banks <steven.mohapibanks@gmail.com>
2012-06-28 11:13:29 +0100

    Added support for ip address and city address mapping

--- lib/gecko-pusher/channel/map.rb
+++ lib/gecko-pusher/channel/map.rb
@@ -5,13 +5,34 @@ module Gecko
 
         def push(*args)
           data = args.map {|arg|
-            if arg.is_a?(String) and arg =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+            if ip_address?(arg)
               {ip: arg}
+            elsif lat_long?(arg)
+              {latitude: arg[0].to_s, longitude: arg[1].to_s}
+            elsif address?(arg)
+              {city: arg}
             end
           }
 
           _push({points: { point: data}})
         end
+
+        protected
+
+          def ip_address?(point_data)
+            point_data.is_a?(String) &&
+              point_data =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+          end
+
+          def lat_long?(point_data)
+            point_data.length == 2 &&
+              point_data.all? { |p| p.is_a? Float }
+          end
+
+          def address?(point_data)
+            point_data.is_a?(Hash) &&
+              point_data.keys.all? {|k| [:city_name, :region_code, :country_code].include?(k)}
+          end
       end
     end
   end
--- spec/lib/gecko-pusher/channel/map_spec.rb
+++ spec/lib/gecko-pusher/channel/map_spec.rb
@@ -18,12 +18,41 @@ describe Gecko::Pusher::Channel::Map do
       data = {
         points: {
           point: [
-            ip: "1.2.3.4"
+            {ip: "1.2.3.4"}
           ]
         }
       }
       stub = stub_gecko_post(WIDGET_KEY, data)
       @channel.push("1.2.3.4")
+      stub.should have_been_requested
+    end
+
+    it "should push a lat/long" do
+      data = {
+        points: {
+          point: [
+            {latitude: "51.0001",longitude: "-51.0001"}
+          ]
+        }
+      }
+      stub = stub_gecko_post(WIDGET_KEY, data)
+      @channel.push([51.0001, -51.0001])
+      stub.should have_been_requested
+    end
+
+    it "should push an address" do
+      data = {
+        points: {
+          point: [
+            {city: {
+              city_name: "London",
+              country_code: "GB"
+            }}
+          ]
+        }
+      }
+      stub = stub_gecko_post(WIDGET_KEY, data)
+      @channel.push({city_name: "London", country_code: "GB"})
       stub.should have_been_requested
     end
   end
